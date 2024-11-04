78e8a03b5f8675be0fb26adb587bce7daaa9eccb
jamekuta175 <jamekuta175@SDS-L-561HC63>
2024-11-04 10:35:09 -0500

    test commit and sync

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace boxing_choice_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pagetxt = new System.Windows.Forms.Label();
+            this.QandA = new System.Windows.Forms.Label();
+            this.yes = new System.Windows.Forms.Button();
+            this.no = new System.Windows.Forms.Button();
+            this.picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pagetxt
+            // 
+            this.pagetxt.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pagetxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pagetxt.ForeColor = System.Drawing.Color.Khaki;
+            this.pagetxt.Location = new System.Drawing.Point(29, 9);
+            this.pagetxt.Name = "pagetxt";
+            this.pagetxt.Size = new System.Drawing.Size(534, 55);
+            this.pagetxt.TabIndex = 0;
+            this.pagetxt.Text = "the bell rings and your opponent is standing there your in a boxing ring";
+            this.pagetxt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // QandA
+            // 
+            this.QandA.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.QandA.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QandA.ForeColor = System.Drawing.Color.Khaki;
+            this.QandA.Location = new System.Drawing.Point(92, 278);
+            this.QandA.Name = "QandA";
+            this.QandA.Size = new System.Drawing.Size(415, 31);
+            this.QandA.TabIndex = 1;
+            this.QandA.Text = " should you uppercut or right hook him?";
+            this.QandA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // yes
+            // 
+            this.yes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.yes.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yes.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.yes.Location = new System.Drawing.Point(12, 312);
+            this.yes.Name = "yes";
+            this.yes.Size = new System.Drawing.Size(267, 58);
+            this.yes.TabIndex = 2;
+            this.yes.Text = "uppercut ";
+            this.yes.UseVisualStyleBackColor = false;
+            this.yes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // no
+            // 
+            this.no.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.no.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.no.Location = new System.Drawing.Point(295, 312);
+            this.no.Name = "no";
+            this.no.Size = new System.Drawing.Size(284, 58);
+            this.no.TabIndex = 3;
+            this.no.Text = "right hook";
+            this.no.UseVisualStyleBackColor = false;
+            this.no.Click += new System.EventHandler(this.no_Click);
+            // 
+            // picture
+            // 
+            this.picture.BackColor = System.Drawing.Color.Transparent;
+            this.picture.Image = global::boxing_choice_game.Properties.Resources.mikey;
+            this.picture.Location = new System.Drawing.Point(119, 77);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(365, 208);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture.TabIndex = 4;
+            this.picture.TabStop = false;
+            this.picture.Click += new System.EventHandler(this.picture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(591, 382);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.no);
+            this.Controls.Add(this.yes);
+            this.Controls.Add(this.QandA);
+            this.Controls.Add(this.pagetxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pagetxt;
+        private System.Windows.Forms.Label QandA;
+        private System.Windows.Forms.Button yes;
+        private System.Windows.Forms.Button no;
+        private System.Windows.Forms.PictureBox picture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace boxing_choice_game
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        SoundPlayer soundPlayer = new SoundPlayer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if(page == 5)
+            {
+                page = 8;
+            }
+            else if (page == 3)
+            {
+                page = 10;
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 11)
+            {
+                page = 15;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                page = 1;
+            }
+            else if (page == 12)
+            {
+                page = 1;
+            }
+            else if (page == 10)
+            {
+                page = 13;
+            }
+            else if (page == 13)
+            {
+                page = 1;
+            }
+            else if (page == 14)
+            {
+                page = 1;
+            }
+            else if (page == 15)
+            {
+                page = 1;
+            }
+
+
+            DisplayPage();
+        }
+
+        private void no_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 4)
+            {
+                page = 7;
+            }
+            else if (page == 10)
+            {
+                page = 12;
+            }
+            else if(page == 2)
+            {
+                page = 5;
+            }
+            else if(page == 5)
+            {
+                page = 9;
+            }
+            else if (page == 11)
+            {
+                page = 14;
+            }
+            else if (page == 3)
+            {
+                page = 11;
+            }
+            else if (page == 11)
+            {
+                page = 15;
+            }
+            else if (page == 6)
+            {
+                Refresh();
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+            else if (page == 7)
+            {
+                Refresh();
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+            else if (page == 8)
+            {
+                Refresh();
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+            else if (page == 9)
+            {
+                Refresh();
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+            else if (page == 12)
+            {
+                Refresh();
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+            else if (page == 13)
+            {
+                Refresh();
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+            else if (page == 14)
+            {
+                Refresh();
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+            else if (page == 15)
+            {
+                Refresh();
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+
+            DisplayPage();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    pagetxt.Text = "the bell rings and your opponent is standing there your in a boxing ring";
+                    picture.Image = Properties.Resources.mikey;
+                    QandA.Text = "should you uppercut or right hook him?";
+                    yes.Text = "uppercut";
+                    no.Text = "right hook ";
+
+                    break;
+
+                case 2:
+                    pagetxt.Text = "you uppercut him he falls down?";
+                    picture.Image = Properties.Resources.upercut;
+                    QandA.Text = "should you kick him while he down?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+
+
+                    soundPlayer = new SoundPlayer(Properties.Resources.Epic_Cinematic_Action_Trailer_Background_Music_No_Copyright_Intense_Teaser_Bgm___ezmp3_cc__);
+                    soundPlayer.Play();
+                    break;
+
+                case 3:
+                    pagetxt.Text = "right hook lands but it doesn't effect him";
+                    picture.Image = Properties.Resources.hook;
+                    QandA.Text = "continue punching or block?";
+                    yes.Text = "block";
+                    no.Text = "punching";
+                    break;
+
+                case 4:
+                    pagetxt.Text = "you kick and stomp on him the ref tries to pull you away";
+                    picture.Image = Properties.Resources.kick;
+                    QandA.Text = "should you fight the ref yes or no?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 5:
+
+                    yes.Enabled = false;
+                    no.Enabled = false;
+
+                    pagetxt.Text = "you wait for him to get up  the count starts";
+                    picture.Image = Properties.Resources.down;
+                    QandA.Text = " ";
+                    yes.Text = " ";
+                    no.Text = " ";
+                    Refresh();
+                    Thread.Sleep(2000);
+
+                    pagetxt.Text = "1!";
+                    Refresh();
+                    Thread.Sleep(550);
+
+                    pagetxt.Text = "2!";
+                    Refresh();
+                    Thread.Sleep(600);
+
+                    pagetxt.Text = "3!";
+                    Refresh();
+                    Thread.Sleep(650);
+
+                    pagetxt.Text = "4!";
+                    Refresh();
+                    Thread.Sleep(700);
+
+                    pagetxt.Text = "5!";
+                    Refresh();
+                    Thread.Sleep(750);
+
+                    yes.Enabled = true;
+                    no.Enabled = true;
+
+                    pagetxt.Text = "he gets back up";
+                    picture.Image = Properties.Resources.mikey;
+                    QandA.Text = "should you kick him while he down?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 6:
+                    pagetxt.Text = "you decide to fight the ref you lose your boxing courier and die alone and sad. Thanks for playing!!!";
+                    picture.Image = Properties.Resources.grave;
+                    QandA.Text = "try again?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 7:
+                    pagetxt.Text = "the ref tackles you and your courier end and you end up homeless and hated. Thanks for playing!!!";
+                    picture.Text = "";
+                    QandA.Text = "try again?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 8:
+                    pagetxt.Text = "you continue the fight and win the championship and your life is happy your opponent never recovers and ends up in prison for running over people drunk killing 5. Thanks for playing!!!";
+                    picture.Image = Properties.Resources.priz;
+                    QandA.Text = "try again?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 9:
+                    pagetxt.Text = "you give up and ends up living a normal life being friends with your opponent. Thanks for playing!!!";
+                    picture.Image = Properties.Resources.happy;
+                    QandA.Text = "try again?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 10:
+                    pagetxt.Text = "you block as he punches as he loses his energy";
+                    picture.Image = Properties.Resources.block;
+                    QandA.Text = "should you fight back";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 11:
+                    pagetxt.Text = "you continue punching he doesn't look so good";
+                    picture.Image = Properties.Resources.ededed;
+                    QandA.Text = "should you tell the ref";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 12:
+                    pagetxt.Text = "you don't fight back and lose people think your a coward and you are hated for the rest of your life. Thanks for playing!!!";
+                    picture.Image = Properties.Resources.downhill;
+                    QandA.Visible = false;
+                    QandA.Text = "try again?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 13:
+                    pagetxt.Text = "you hit him in the side breaking his ribs and wining. Thanks for playing!!!";
+                    picture.Image = Properties.Resources.win;
+                    QandA.Text = "try again?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+
+                case 14:
+                    pagetxt.Text = "you knock him out seeing that he isn't going back up you dont find out untill years later but after that fight theft him in a vegetative state. Thanks for playing!!!";
+                    picture.Image = Properties.Resources.weelend;
+                    QandA.Text = "try again?";
+                    yes.Text = "yes"; 
+                    no.Text = "no";
+                    break;
+
+                case 15:
+                    pagetxt.Text = "you tell the ref and he steps in you win and never see your opponent again. Thanks for playing!!!";
+                    picture.Image = Properties.Resources.win;
+                    QandA.Text = "try again?";
+                    yes.Text = "yes";
+                    no.Text = "no";
+                    break;
+            }
+        }
+
+        private void picture_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
